c2c597f6b10bf04709f362993007b9bffe9378b7
Anshuman Yadav <ianshumanyadav@gmail.com>
2021-11-21 19:30:57 +0530

    added Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <p className="nav-title">Pokemon Cards</p>
+            <a href="">API used</a>
+        </div>
+    )
+}
+
+export default Navbar;
