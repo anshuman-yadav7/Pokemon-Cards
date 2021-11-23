80aa68eb8cbaa827f425b7f03f083bcc14d87042
Anshuman Yadav <ianshumanyadav@gmail.com>
2021-11-23 22:43:17 +0530

    Updated card component styles

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -6,10 +6,11 @@ function Card({pokemon}) {
         <div className="card">
             <div className="card-img">
                 <img src={pokemon.sprites.front_default} alt="" />
+                <p className="card-name">
+                    {pokemon.name}
+                </p>
             </div>
-            <div className="card-name">
-                {pokemon.name}
-            </div>
+            
             <div className="card-types">
                 {
                     pokemon.types.map(type => {
@@ -22,15 +23,15 @@ function Card({pokemon}) {
                 }
             </div>
             <div className="card-info">
-                <div className="card-data card-data--weight">
+                <div className="card-data">
                     <p className="title">Weight</p>
                     <p>{pokemon.weight}</p>
                 </div>
-                <div className="card-data card-data--weight">
+                <div className="card-data">
                     <p className="title">Height</p>
                     <p>{pokemon.height}</p>
                 </div>
-                <div className="card-data card-data--ability">
+                <div className="card-data">
                     <p className="title">Ability</p>
                     <p>{pokemon.abilities[0].ability.name}</p>
                 </div>
